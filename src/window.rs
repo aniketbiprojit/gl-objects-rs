15127831fa6d8c21ab7396db60c39f62e5f01e72
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-10-01 21:10:56 +0530

    feat: init glfw

--- src/window.rs
+++ src/window.rs
@@ -1,19 +1,90 @@
+use glfw::Context;
 use glow::HasContext;
+use std::{ffi::c_void, sync::mpsc::channel};
 pub struct Window<WindowContext, WindowHandle> {
     pub width: u32,
     pub height: u32,
     pub title: String,
     pub ctx: Option<WindowContext>,
-    pub window_handle: Option<WindowHandle>,
+    pub internal_handle: Option<WindowHandle>,
 }
 
-pub trait WindowTrait<WindowHandle> {
+pub trait WindowTrait<WindowContext, WindowHandle> {
+    fn new(width: u32, height: u32, title: String) -> Window<WindowContext, WindowHandle> {
+        Window {
+            width,
+            height,
+            title: format!("{}", title),
+            ctx: None,
+            internal_handle: None,
+        }
+    }
     fn create_display(&mut self);
-    fn load_with(&mut self, window: &WindowHandle, s: &str) -> *const std::ffi::c_void;
-    fn event_loop(&mut self) {}
+    fn load_with(&mut self, window: &mut WindowHandle, s: &str) -> *const std::ffi::c_void;
+    fn event_loop(&mut self);
 }
 
-impl WindowTrait<sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window> {
+impl WindowTrait<glfw::Glfw, glfw::Window> for Window<glfw::Glfw, glfw::Window> {
+    fn create_display(&mut self) {
+        let mut glfw: glfw::Glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+        glfw.window_hint(glfw::WindowHint::ContextVersionMajor(4));
+        glfw.window_hint(glfw::WindowHint::ContextVersionMinor(1));
+        glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+            glfw::OpenGlProfileHint::Core,
+        ));
+
+        glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+        let (mut window, ..) = glfw
+            .create_window(
+                self.width,
+                self.height,
+                &self.title,
+                glfw::WindowMode::Windowed,
+            )
+            .expect("Failed to create GLFW window.");
+
+        window.set_all_polling(true);
+        window.make_current();
+
+        let gl = unsafe { glow::Context::from_loader_function(|s| self.load_with(&mut window, s)) };
+
+        println!("{:?}", gl.version());
+
+        self.ctx = Some(glfw);
+        self.internal_handle = Some(window);
+    }
+
+    fn event_loop(&mut self) {
+        let mut glfw = self.ctx.take().unwrap();
+        let window = self.internal_handle.take().unwrap();
+
+        let (sender, receiver): (
+            std::sync::mpsc::Sender<(f64, glfw::WindowEvent)>,
+            std::sync::mpsc::Receiver<(f64, glfw::WindowEvent)>,
+        ) = channel();
+        unsafe {
+            glfw::ffi::glfwSetWindowUserPointer(
+                window.window_ptr(),
+                std::mem::transmute(Box::new(sender)),
+            );
+        }
+
+        while !window.should_close() {
+            glfw.poll_events();
+            for (_, event) in glfw::flush_messages(&receiver) {
+                println!("{:?}", event);
+            }
+        }
+    }
+
+    fn load_with(&mut self, window: &mut glfw::Window, s: &str) -> *const std::ffi::c_void {
+        window.get_proc_address(s) as *const c_void
+    }
+}
+
+impl WindowTrait<sdl2::Sdl, sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window> {
     fn create_display(&mut self) {
         let ctx = sdl2::init().unwrap();
 
@@ -25,7 +96,7 @@ impl WindowTrait<sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window>
         gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
         gl_attr.set_context_flags().debug().set();
 
-        let window = video_subsystem
+        let mut window = video_subsystem
             .window(&self.title, self.width, self.height)
             .allow_highdpi()
             .opengl()
@@ -35,7 +106,7 @@ impl WindowTrait<sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window>
             .unwrap();
 
         let gl_context = window.gl_create_context().unwrap();
-        let gl = unsafe { glow::Context::from_loader_function(|s| self.load_with(&window, s)) };
+        let gl = unsafe { glow::Context::from_loader_function(|s| self.load_with(&mut window, s)) };
 
         println!("{:?}", gl.version());
 
@@ -44,7 +115,7 @@ impl WindowTrait<sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window>
         window.subsystem().gl_set_swap_interval(1).unwrap();
 
         self.ctx = Some(ctx);
-        self.window_handle = Some(window);
+        self.internal_handle = Some(window);
     }
 
     fn event_loop(&mut self) {
@@ -57,7 +128,7 @@ impl WindowTrait<sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window>
 
         'event_loop: loop {
             for event in event_pump.poll_iter() {
-                self.window_handle
+                self.internal_handle
                     .as_ref()
                     .expect("REASON")
                     .gl_swap_window();
@@ -68,7 +139,7 @@ impl WindowTrait<sdl2::video::Window> for Window<sdl2::Sdl, sdl2::video::Window>
         }
     }
 
-    fn load_with(&mut self, window: &sdl2::video::Window, s: &str) -> *const std::ffi::c_void {
+    fn load_with(&mut self, window: &mut sdl2::video::Window, s: &str) -> *const std::ffi::c_void {
         window.subsystem().gl_get_proc_address(s) as _
     }
 }
