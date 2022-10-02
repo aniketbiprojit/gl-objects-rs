9558291e4bacc491748da183d4013fa0d83ea441
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-10-02 15:01:01 +0530

    fix: window privatization

--- examples/glfw.rs
+++ examples/glfw.rs
@@ -1,18 +1,3 @@
-use gl_object::window::WindowTrait;
-
 fn main() {
-    let mut handle = gl_object::window::Window::<glfw::Glfw, glfw::Window>::new(
-        800,
-        600,
-        format!("GLFW {}", "Window".to_string()),
-    );
-
-    handle.create_display();
-    let rectangle = &mut gl_object::primitives::rectangle::Rectangle::new(
-        200,
-        200,
-        "resources/shader_with_matrix.shader",
-    );
-
-    handle.render(&mut vec![rectangle])
+    gl_object::glfw_example();
 }
--- examples/sdl2.rs
+++ examples/sdl2.rs
@@ -1,18 +1,5 @@
-use gl_object::window::WindowTrait;
+use gl_object::sdl2_example;
 
 fn main() {
-    let mut handle = gl_object::window::Window::<sdl2::Sdl, sdl2::video::Window>::new(
-        800,
-        600,
-        format!("SDL {}", "Window".to_string()),
-    );
-
-    handle.create_display();
-    let rectangle = &mut gl_object::primitives::rectangle::Rectangle::new(
-        200,
-        200,
-        "resources/shader_with_matrix.shader",
-    );
-
-    handle.render(&mut vec![rectangle])
+    sdl2_example();
 }
