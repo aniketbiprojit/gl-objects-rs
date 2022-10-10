b2f820311b221e9fae33df3014efb17c73818221
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-10-11 05:06:58 +0530

    feat: add glium target

--- src/window.rs
+++ src/window.rs
@@ -3,8 +3,6 @@ use glium::Surface;
 use glow::HasContext;
 
 use std::sync::mpsc::channel;
-use std::{cell::RefCell, rc::Rc};
-use takeable_option::Takeable;
 
 use crate::object::{OpenGLObjectTrait, TestingEvent};
 
@@ -15,7 +13,6 @@ pub struct Window<WindowContext, WindowHandle> {
     pub ctx: Option<Box<WindowContext>>,
     pub internal_handle: Option<*mut WindowHandle>,
     pub gl: Option<Box<glow::Context>>,
-    pub frame: Rc<RefCell<Takeable<glium::Frame>>>,
     // sdl2 specific
     #[cfg(feature = "sdl2")]
     gl_context: Option<Box<sdl2::video::GLContext>>,
@@ -29,7 +26,6 @@ pub trait WindowTrait<WindowContext, WindowHandle> {
             title: format!("{}", title),
             ctx: None,
             internal_handle: None,
-            frame: Rc::new(RefCell::new(Takeable::new_empty())),
             gl: None,
             #[cfg(feature = "sdl2")]
             gl_context: None,
@@ -51,7 +47,9 @@ impl WindowTrait<glfw::Glfw, glfw::Window> for Window<glfw::Glfw, glfw::Window> 
         }
         let gl = self.gl.as_ref().unwrap();
         let glfw = self.ctx.as_mut().unwrap();
-        let window = unsafe { &mut *self.internal_handle.unwrap() };
+        let raw_handle = self.internal_handle.unwrap();
+        let window = unsafe { &mut *raw_handle };
+
         let (sender, receiver): (
             std::sync::mpsc::Sender<(f64, glfw::WindowEvent)>,
             std::sync::mpsc::Receiver<(f64, glfw::WindowEvent)>,
@@ -64,22 +62,83 @@ impl WindowTrait<glfw::Glfw, glfw::Window> for Window<glfw::Glfw, glfw::Window> 
             );
         }
 
+        struct Backend {
+            gl_window: *mut glfw::Window,
+        }
+        unsafe impl<'a> glium::backend::Backend for Backend {
+            fn swap_buffers(&self) -> Result<(), glium::SwapBuffersError> {
+                let window = unsafe { &mut *self.gl_window };
+
+                Ok(window.swap_buffers())
+            }
+
+            unsafe fn get_proc_address(&self, symbol: &str) -> *const std::os::raw::c_void {
+                let window = &mut *self.gl_window;
+
+                window.get_proc_address(symbol)
+            }
+
+            fn get_framebuffer_dimensions(&self) -> (u32, u32) {
+                let window = unsafe { &mut *self.gl_window };
+                let x = window.get_framebuffer_size();
+                // .get_framebuffer_size();
+                (x.0 as u32, x.1 as u32)
+            }
+
+            fn is_current(&self) -> bool {
+                let window = unsafe { &mut *self.gl_window };
+
+                window.is_current()
+            }
+
+            unsafe fn make_current(&self) {
+                let window = &mut *self.gl_window;
+
+                window.make_current()
+            }
+        }
+
+        let gl_window = raw_handle;
+
+        // target.borrow_mut().clear_color(0.0, 1.0, 0.0, 1.0);
+        // target.borrow_mut().clear_depth(1.0);
+
+        // self.frame = target.clone();
+
+        // self.render(objects);
+
+        let mut frame_count = 0;
+        let time = std::time::Instant::now();
+
         while !window.should_close() || true {
+            // FIXME - hacky. probably a bad idea to create the context inside
+            // the loop but the fps was still around 60ish
+            println!(
+                "frames per second {}",
+                frame_count as f32 / time.elapsed().as_secs_f32()
+            );
+            let glium_context = unsafe {
+                // The first parameter is our backend.
+                //
+                // The second parameter tells glium whether or not it should regularly call `is_current`
+                // on the backend to make sure that the OpenGL context is still the current one.
+                //
+                // It is recommended to pass `true`, but you can pass `false` if you are sure that no
+                // other OpenGL context will be made current in this thread.
+                let backend = Backend {
+                    gl_window: gl_window,
+                };
+                glium::backend::Context::new(backend, true, Default::default()).unwrap()
+            };
+
             if window.should_close() {
-                let frame = Takeable::try_take(&mut self.frame.borrow_mut());
-                if frame.is_some() {
-                    match frame.unwrap().set_finish() {
-                        Ok(_) => {}
-                        Err(_) => {}
-                    };
-                } else {
-                    println!("frame is non");
-                }
+                println!("frame is non");
                 window.set_should_close(true);
 
                 break;
             } else {
                 glfw.poll_events();
+
                 let mut test_event = None;
                 for (_, event) in glfw::flush_messages(&receiver) {
                     if let glfw::WindowEvent::Size(x, y) = event {
@@ -99,9 +158,16 @@ impl WindowTrait<glfw::Glfw, glfw::Window> for Window<glfw::Glfw, glfw::Window> 
                     }
                 }
 
+                let mut target = glium::Frame::new(
+                    glium_context.clone(),
+                    glium_context.get_framebuffer_dimensions(),
+                );
+
                 unsafe {
                     gl.clear_color(0.1, 0.2, 0.3, 1.0);
                     gl.clear(glow::COLOR_BUFFER_BIT);
+                    target.clear_color(0.0, 1.0, 0.0, 1.0);
+                    target.clear_depth(1.0);
                 }
                 for elem in objects.into_iter() {
                     elem.attach(gl);
@@ -120,7 +186,12 @@ impl WindowTrait<glfw::Glfw, glfw::Window> for Window<glfw::Glfw, glfw::Window> 
                 unsafe {
                     gl.viewport(0, 0, x, y);
                 }
-                window.swap_buffers();
+                // window.swap_buffers();
+                frame_count += 1;
+
+                target.finish().unwrap_or_else(|e| {
+                    println!("Error: {}", e);
+                });
             }
         }
         for elem in objects.into_iter() {
@@ -167,69 +238,6 @@ impl WindowTrait<glfw::Glfw, glfw::Window> for Window<glfw::Glfw, glfw::Window> 
         let raw_handle = Box::into_raw(Box::new(window));
         self.internal_handle = Some(raw_handle);
         self.gl = Some(Box::new(gl));
-
-        struct Backend {
-            gl_window: *mut glfw::Window,
-        }
-        unsafe impl<'a> glium::backend::Backend for Backend {
-            fn swap_buffers(&self) -> Result<(), glium::SwapBuffersError> {
-                let window = unsafe { &mut *self.gl_window };
-
-                Ok(window.swap_buffers())
-            }
-
-            unsafe fn get_proc_address(&self, symbol: &str) -> *const std::os::raw::c_void {
-                let window = &mut *self.gl_window;
-
-                window.get_proc_address(symbol)
-            }
-
-            fn get_framebuffer_dimensions(&self) -> (u32, u32) {
-                let window = unsafe { &mut *self.gl_window };
-                let x = window.get_framebuffer_size();
-                // .get_framebuffer_size();
-                (x.0 as u32, x.1 as u32)
-            }
-
-            fn is_current(&self) -> bool {
-                let window = unsafe { &mut *self.gl_window };
-
-                window.is_current()
-            }
-
-            unsafe fn make_current(&self) {
-                let window = &mut *self.gl_window;
-
-                window.make_current()
-            }
-        }
-
-        let gl_window = raw_handle;
-        let context = unsafe {
-            // The first parameter is our backend.
-            //
-            // The second parameter tells glium whether or not it should regularly call `is_current`
-            // on the backend to make sure that the OpenGL context is still the current one.
-            //
-            // It is recommended to pass `true`, but you can pass `false` if you are sure that no
-            // other OpenGL context will be made current in this thread.
-            let backend = Backend {
-                gl_window: gl_window,
-            };
-            glium::backend::Context::new(backend, true, Default::default())
-        }
-        .unwrap();
-
-        let target = Rc::new(RefCell::new(Takeable::new(glium::Frame::new(
-            context.clone(),
-            context.get_framebuffer_dimensions(),
-        ))));
-        target.borrow_mut().clear_color(0.0, 1.0, 0.0, 1.0);
-        target.borrow_mut().clear_depth(1.0);
-
-        self.frame = target.clone();
-
-        // self.render(objects);
     }
 
     fn load_with(&mut self, s: &str) -> *const std::ffi::c_void {
@@ -273,13 +281,13 @@ impl WindowTrait<sdl2::Sdl, sdl2::video::Window> for Window<sdl2::Sdl, sdl2::vid
         self.gl_context = Some(Box::new(gl_context));
 
         self.ctx = Some(Box::new(ctx));
-        self.internal_handle = Some(Box::new(window));
+        self.internal_handle = Some(Box::into_raw(Box::new(window)));
         self.gl = Some(Box::new(gl));
         // self.render(objects);
     }
 
     fn load_with(&mut self, s: &str) -> *const std::ffi::c_void {
-        let window = self.internal_handle.as_ref().unwrap();
+        let window = unsafe { &*self.internal_handle.unwrap() };
 
         window.subsystem().gl_get_proc_address(s) as _
     }
@@ -295,10 +303,56 @@ impl WindowTrait<sdl2::Sdl, sdl2::video::Window> for Window<sdl2::Sdl, sdl2::vid
         if self.ctx.is_none() {
             panic!("ctx is none");
         }
+
         unsafe {
             let gl = self.gl.as_ref().unwrap();
             let ctx = self.ctx.as_ref().unwrap();
-            let window = self.internal_handle.as_ref().unwrap();
+            let raw_handle = self.internal_handle.unwrap();
+            let window = { &*raw_handle };
+
+            struct Backend {
+                gl_window: *mut sdl2::video::Window,
+            }
+            unsafe impl<'a> glium::backend::Backend for Backend {
+                fn swap_buffers(&self) -> Result<(), glium::SwapBuffersError> {
+                    let window = unsafe { &*self.gl_window };
+
+                    Ok(window.gl_swap_window())
+                }
+
+                unsafe fn get_proc_address(&self, symbol: &str) -> *const std::os::raw::c_void {
+                    let window = &*self.gl_window;
+
+                    window.subsystem().gl_get_proc_address(symbol) as _
+                }
+
+                fn get_framebuffer_dimensions(&self) -> (u32, u32) {
+                    let window = unsafe { &*self.gl_window };
+                    window.drawable_size()
+                }
+
+                fn is_current(&self) -> bool {
+                    true
+                }
+
+                unsafe fn make_current(&self) {}
+            }
+
+            let gl_window = raw_handle;
+
+            let glium_context = {
+                // The first parameter is our backend.
+                //
+                // The second parameter tells glium whether or not it should regularly call `is_current`
+                // on the backend to make sure that the OpenGL context is still the current one.
+                //
+                // It is recommended to pass `true`, but you can pass `false` if you are sure that no
+                // other OpenGL context will be made current in this thread.
+                let backend = Backend {
+                    gl_window: gl_window,
+                };
+                glium::backend::Context::new(backend, true, Default::default()).unwrap()
+            };
 
             window
                 .gl_make_current(&self.gl_context.as_ref().unwrap())
@@ -341,7 +395,15 @@ impl WindowTrait<sdl2::Sdl, sdl2::video::Window> for Window<sdl2::Sdl, sdl2::vid
                     }
                 }
 
+                let mut target = glium::Frame::new(
+                    glium_context.clone(),
+                    glium_context.get_framebuffer_dimensions(),
+                );
+
                 gl.clear(glow::COLOR_BUFFER_BIT);
+
+                target.clear_color(0.0, 1.0, 0.0, 1.0);
+                target.clear_depth(1.0);
 
                 for elem in objects.into_iter() {
                     elem.attach(gl);
@@ -357,7 +419,8 @@ impl WindowTrait<sdl2::Sdl, sdl2::video::Window> for Window<sdl2::Sdl, sdl2::vid
                     elem.render(gl);
                 }
 
-                window.gl_swap_window();
+                // window.gl_swap_window();
+                target.finish().unwrap();
             }
             for elem in objects.into_iter() {
                 elem.detach(gl);
